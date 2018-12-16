46563f119b10f33788009263168638625b7f4dfc
Thom <thom@homeostasie.eu>
2018-12-16 23:08:24 +0100

    3è : ajout des notes et des diagrammes pour l'éval 2.

--- 3_troisieme/_scripts/bilan-eval.R
+++ 3_troisieme/_scripts/bilan-eval.R
@@ -1,4 +1,5 @@
-# Exec with : R -q --vanilla < bilan-eval.R 
+
+# Exec with : R -q --vanilla < bilan-eval.R
 
 # Notes sur l'évaluation de statistiques
 
@@ -11,8 +12,26 @@ med = median(ie1)
 moy = mean(ie1)
 
 
-pdf(file="ie1.pdf")
-    hist(ie1, col="grey", main="Evaluation 1 - Statistiques",
+#pdf(file="ie1.pdf")
+#    hist(ie1, col="grey", main="Evaluation 1 - Statistiques",
+#     xlab=NULL, ylab=NULL)
+#     legend("topleft",c("Etendue = ", etendu, "Moyenne =", moy, "Mediane =", med) )
+
+# Notes sur l'évaluation fonctions
+
+
+ie2 = c(17.5, 18, 13.5, 3.5, 7.5, 11, 6, 15, 20, 1, 14,
+        13.5, 13.5, 6.5, 20, 20, 13, 8, 4)
+
+
+etendu = max(ie2) - min(ie2)
+med = median(ie2)
+moy = mean(ie2)
+
+
+pdf(file="ie2.pdf")
+    hist(ie2, col="grey", main="Evaluation - Fonction",
      xlab=NULL, ylab=NULL)
      legend("topleft",c("Etendue = ", etendu, "Moyenne =", moy, "Mediane =", med) )
+
 
